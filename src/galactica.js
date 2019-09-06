28e9b673aabb21a8b8ed8c025450becb5f49ad16
Student <example@example.com>
2019-09-06 16:28:55 -0700

    Add one function to calculate left years on Earth

--- src/galactica.js
+++ src/galactica.js
@@ -7,27 +7,32 @@ Galactica.prototype.surpassedAge = function () {
     return this.age - 71
   }
 }
-Galactica.prototype.marsAge = function () {
-  return (this.age / 1.88);
+  Galactica.prototype.leftAgeEarth = function () {
+    if (this.age<71){
+    return 71 - this.age
+  }
 }
-Galactica.prototype.mercuryAge = function () {
-  return this.age / .24
-}
-Galactica.prototype.venusAge = function () {
-  return this.age / .62
-}
-Galactica.prototype.jupiterAge = function () {
-  return this.age / 11.86
-}
-Galactica.prototype.leftMarsAge = function () {
-  return (71 - this.age) / 1.88
-}
-Galactica.prototype.leftMercuryAge = function () {
-  return (71 - this.age) / .24
-}
-Galactica.prototype.leftVenusAge = function () {
-  return (71 - this.age) / .62
-}
-Galactica.prototype.leftJupiterAge = function () {
-  return (71 - this.age) / 11.86
-}
+  Galactica.prototype.marsAge = function () {
+    return this.age / 1.88
+  }
+  Galactica.prototype.mercuryAge = function () {
+    return this.age / .24
+  }
+  Galactica.prototype.venusAge = function () {
+    return this.age / .62
+  }
+  Galactica.prototype.jupiterAge = function () {
+    return this.age / 11.86
+  }
+  Galactica.prototype.leftMarsAge = function () {
+    return (71 - this.age) / 1.88
+  }
+  Galactica.prototype.leftMercuryAge = function () {
+    return (71 - this.age) / .24
+  }
+  Galactica.prototype.leftVenusAge = function () {
+    return (71 - this.age) / .62
+  }
+  Galactica.prototype.leftJupiterAge = function () {
+    return (71 - this.age) / 11.86
+  }
