8118343b0aadfd556db0402bd07959d32c4da7f7
Student <example@example.com>
2019-09-06 11:23:17 -0700

    Add UI logic.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import { Galactica } from './galactica.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var inputtedAge = $("input#userage").val();
+    var newGalactica = new Galactica (inputtedAge);
+     $("#mars-age").text(newGalactica.marsAge());
+
+  });
+});
