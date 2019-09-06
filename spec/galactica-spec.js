d28efab1063bfb53375d8c03d3f6f8ec48bc807a
Student <example@example.com>
2019-09-06 14:50:33 -0700

    Tested mercuryAge function with succes.

--- spec/galactica-spec.js
+++ spec/galactica-spec.js
@@ -1,21 +1,20 @@
 import { Galactica } from './../src/galactica.js';
 describe('Galactica', function() {
 
-    let testGalactica = new Galactica("90");
-    it('surpassedAge function should return 19', function() {
-      expect(testGalactica.surpassedAge()).toEqual(19);
-    });
+  let testGalactica = new Galactica("90");
+  it('surpassedAge function should return 19', function() {
+    expect(testGalactica.surpassedAge()).toEqual(19);
   });
-//     it('marsAge function should return 47.8728404', function() {
-//       expect(testGalactica.age).toEqual(47.8728404);
-//     });
-//     it('mercuryAge function should return 375', function() {
-//       expect(testGalactica.age).toEqual(375);
-//     });
-//     it('venusAge function should return 145.1612', function() {
-//       expect(testGalactica.age).toEqual(145.1612);
-//     });
-//     it('jupiterAge function should return 7.588', function() {
-//       expect(testGalactica.age).toEqual(7.588);
-// });
-// });
+  it('marsAge function should return 47.8728404', function() {
+    expect(testGalactica.marsAge()).toEqual(47.8728404);
+  });
+  it('mercuryAge function should return 375', function() {
+    expect(testGalactica.mercuryAge()).toEqual(375);
+  });
+  it('venusAge function should return 145.1612', function() {
+    expect(testGalactica.venusAge()).toEqual(145.1612);
+  });
+  it('jupiterAge function should return 7.588', function() {
+    expect(testGalactica.jupiterAge()).toEqual(7.588);
+  });
+});
